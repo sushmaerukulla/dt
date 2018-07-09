ef413a8694b418c51147f28c8a9224bcb62fb1dd
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 22:05:05 +0530

    Max b/w two numbers

---
+++ Max.java
@@ -0,0 +1,17 @@
+class Max
+{
+public static void main(String args[])
+{
+int num1,num2;
+System.out.println("enter two numbers");
+Scanner s=new Scanner(System.in);
+num1=s.nextInt();
+num2=s.nextInt();
+if(num1>num2)
+{
+System.out.println("num1 is greater");
+}
+else
+System.out.println("num 2 is greater");
+}
+}
