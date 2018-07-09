1a7cf32726ea2f2e4fb2ffe4b96b883f289a1c08
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 21:11:57 +0530

    4) Addition of two numbers

---
+++ Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Add
+{
+public static void main(String args[])
+{
+int num1,num2,result;
+Scanner s= new Scanner(System.in);
+num1=s.nextInt();
+num2=s.nextInt();
+result=num1+num2;
+System.out.println("Addition="+result);
+}
+}
