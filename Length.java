231065b5c4b4f519227794fb56be6c32a4e63e96
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 21:44:08 +0530

    Length of a string

---
+++ Length.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Length
+{
+public static void main(String args[])
+{
+String str;
+int length;
+System.out.println("enter string");
+Scanner s=new Scanner(System.in);
+str=s.nextLine();
+length=str.length();
+System.out.println(length);
+}
+}
