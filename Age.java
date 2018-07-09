f2d126e599a48ddd6eaa5c7e3f1b98b62a1cb916
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 21:13:33 +0530

    5) Read age from user and print eligibility for voting

---
+++ Age.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Age
+{
+public static void main(String args[])
+{
+int age;
+Scanner s=new Scanner(System.in);
+age=s.nextInt();
+if(age>18)
+{
+System.out.println("eligible");
+}
+else 
+System.out.println("not eligible");
+}
+}
