e4ad888cb7c800225b5a9dcd83155e900552181b
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 22:06:18 +0530

    Minimum of two numbers

---
+++ Min.java
@@ -0,0 +1,17 @@
+class Min
+{
+public static void main(String args[])
+{
+int num1,num2;
+System.out.println("enter two numbers");
+Scanner s=new Scanner(System.in);
+num1=s.nextInt();
+num2=s.nextInt();
+if(num1<num2)
+{
+System.out.println("num1 is min");
+}
+else
+System.out.println("num 2 is min");
+}
+}
