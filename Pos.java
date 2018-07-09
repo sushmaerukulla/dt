edf2422dadd94d250261ea6d0d96bfe2080c575c
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 22:09:41 +0530

    Print positive or negative

---
+++ Pos.java
@@ -0,0 +1,17 @@
+class Pos
+{
+public static void main(String args[])
+{
+int num;
+System.out.println("enter two numbers");
+Scanner s=new Scanner(System.in);
+num=s.nextInt();
+
+if(num>0)
+{
+System.out.println("positive");
+}
+else
+System.out.println("negative");
+}
+}
