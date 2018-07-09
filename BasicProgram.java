5dd7035437b4f513f830a151901e0bd806ec0eb4
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 21:10:27 +0530

    3) Read a string and print it twice

--- BasicProgram.java
+++ BasicProgram.java
@@ -1,16 +1,14 @@
-//2)Read a character and print it twicess
+//2)Read a string and print it twicess
 import java.util.*;
 import java.lang.*;
 class BasicProgram
 {
 public static void main(String args[])
 {
-String str;
-char ch;
+String str="hello";
 Scanner s=new Scanner(System.in);
 str=s.nextLine();
-ch=str.charAt(0);
-System.out.println(ch);
-System.out.println(ch);
+System.out.println(str);
+System.out.println(str);
 }
 }
