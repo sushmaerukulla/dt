ff0c83bc5945b9e2d2b098d64d639f7edccadb22
sushmaerukulla <sushmaerukulla111@gmail.com>
2018-07-09 22:11:49 +0530

    Divisible either by 5 or 11

---
+++ Div.java
@@ -0,0 +1,16 @@
+class Div
+{
+public static void main(String args[])
+{
+int num;
+System.out.println("enter two numbers");
+Scanner s=new Scanner(System.in);
+num=s.nextInt();
+
+if(num%5==0 || num%11==0)
+{
+System.out.println("number divisible either by 5 or 11);
+}
+}
+}
+
